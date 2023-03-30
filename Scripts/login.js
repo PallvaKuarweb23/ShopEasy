7bfdae6d817c386fabb20a7ef5fe2c82f382c01b
KrishnaVaishnav98 <krishnavaishnav125@gmail.com>
2023-03-30 21:38:39 +0530

    day3 payment

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,27 +1,34 @@
-let form=document.querySelector("form")
-let email=document.getElementById("email")
-let pass=document.getElementById("password")
-let h1=document.getElementById("heading")
-let data=JSON.parse(localStorage.getItem("User-data"))
+let form = document.querySelector("form");
+let email = document.getElementById("email");
+let pass = document.getElementById("password");
+let h1 = document.getElementById("heading");
+let data = JSON.parse(localStorage.getItem("User-data"));
 // console.log(data)
 
-form.addEventListener("submit",function(e){
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   // console.log(email.value,pass.value)
-  let print=false
-  for(let i=0;i<data.length;i++){
-    
-    if(email.value===data[i].email && pass.value===data[i].password){
-        print=true;
-        break;
+  let print = false;
+  for (let i = 0; i < data.length; i++) {
+    if (email.value === data[i].email && pass.value === data[i].password) {
+      print = true;
+      break;
     }
   }
-  if (print==true){
+  if (print == true) {
     // h1.innerText="Sign in Successful"
-    window.location.href="../index.html"
-  }
-  
-  else
- alert("Please enter correct Details")
+    window.location.href = "../index.html";
+  } else alert("Please enter correct Details");
+});
 
-})
+// redirecting to the pages
+function reisterpageY() {
+  window.location.href = "../html/register.html";
+}
+
+function facebookpage() {
+  window.location.href = "https://www.facebook.com/";
+}
+function googlepage() {
+  window.location.href = "https://accounts.google.com/";
+}
--- Scripts/register.js
+++ Scripts/register.js
@@ -1,19 +1,30 @@
-let formdata=JSON.parse(localStorage.getItem("User-data"))||[]
-  // let formdata=[]
-  let form=document.querySelector("form")
-  let name=document.getElementById("name")
-  let surname=document.getElementById("surname")
-  let email=document.getElementById("email")
-  let pass=document.getElementById("password")
-  form.addEventListener("submit",(e)=>{
-     e.preventDefault();
-    let details={
-      name:name.value,
-      surname:surname.value,
-      email:email.value,
-      password:pass.value
-    }
-    formdata.push(details)
-    localStorage.setItem("User-data",JSON.stringify(formdata))
-    location.replace("./login.html")
-  })
+let formdata = JSON.parse(localStorage.getItem("User-data")) || [];
+// let formdata=[]
+let form = document.querySelector("form");
+let name = document.getElementById("name");
+let surname = document.getElementById("surname");
+let email = document.getElementById("email");
+let pass = document.getElementById("password");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let details = {
+    name: name.value,
+    surname: surname.value,
+    email: email.value,
+    password: pass.value,
+  };
+  formdata.push(details);
+  localStorage.setItem("User-data", JSON.stringify(formdata));
+  location.replace("./login.html");
+});
+
+// buttons
+function loginpage() {
+  window.location.href = "../html/login.html";
+}
+function facebookpage() {
+  window.location.href = "https://www.facebook.com/";
+}
+function googlepage() {
+  window.location.href = "https://accounts.google.com/";
+}
