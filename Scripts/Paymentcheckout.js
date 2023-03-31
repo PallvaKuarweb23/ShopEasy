2ad7916956de1f4c4cae2d4ef324e03a2dbdfb33
KrishnaVaishnav98 <krishnavaishnav125@gmail.com>
2023-03-31 13:06:42 +0530

    Changes by Krishna

--- Scripts/Paymentcheckout.js
+++ Scripts/Paymentcheckout.js
@@ -1,40 +1,72 @@
-
+// ----------------  for card payment ---------------
 
 let h1 = document.querySelector("h1")
-let number = document.getElementById("number");
+// let number = document.getElementById("number");
 let name = document.getElementById("name");
 let Cvv = document.getElementById("cvv");
 let submitBtn = document.getElementById("SubmitOtp");
 let Otp1 = document.getElementById("otp");
-let generate = document.getElementById("generate")
+let generate = document.getElementById("generate");
+let PaymentDetails = document.getElementById("PaymentDetails");
+
+
+let  address = document.getElementById("address");
+let  city = document.getElementById("city");
+let  state = document.getElementById("state");
+let  country = document.getElementById("country");
+let  number = document.getElementById("number");
+let  expiryDate = document.getElementById("expiryDate");
+let  cvv = document.getElementById("cvv");
+
+
 
 var otp = 0;
 generate.addEventListener("click", ()=>{
 
+    if(address.value == "" || city.value == "" || state.value == ""  || country.value == "" || number.value == "" || expiryDate.value == "" ||  cvv.value == "")  {
+        alert("Please enter all requied field");
+    }
 
-    otp = Math.floor(Math.random() * 10000);
+    else{
+        function getOTP() {
+            otp = Math.floor(Math.random() * 10000)
+            otp = "" + otp
+            if (otp.length == 4) {
+                return otp;
+            }  else {
+                return getOTP()
+            }
+        
+        }
+    
+        otp = getOTP()
+    
+        setTimeout(function () {
+    
+           alert(`Your otp is ${otp}`)
+    
+         }, 2000)
+    
+        return otp
+    }
 
-    setTimeout(function () {
-
-       alert(`Your otp is ${otp}`)
-
-     }, 2000)
-
-    return otp
 })
 
 
-
 submitBtn.addEventListener("click", () => {
+
+
     if (otp == Number(Otp1.value)) {
         alert("Payment Successful 🎉")
         
+        PaymentDetails.style.visibility = "hidden";
+
         h1.innerText = " Thank You for Shopping..!!!"
         setTimeout(function () {
 
            window.location.href = "../index.html"
 
-        }, 2500)
+        }, 3000)
 
     } else {
         alert("Invalid OTP")
@@ -43,3 +75,74 @@ submitBtn.addEventListener("click", () => {
 })
 
 
+// ----------------------- for upi payment----------------------------------
+
+let byUpi = document.getElementById("byUpi");
+let payByUpi = document.getElementById("payByUpi");
+
+let verifyUpi = document.getElementById("verifyUpi");
+let upiId = document.getElementById("upiId");
+
+
+let  address2 = document.getElementById("address2");
+let  city2 = document.getElementById("city2");
+let  state2 = document.getElementById("state2");
+let  country2 = document.getElementById("country2");
+
+byUpi.addEventListener("click",()=>{
+    payByUpi.style.visibility = "visible";
+    PaymentDetails.style.visibility = "hidden";
+})
+
+verifyUpi.addEventListener("click",()=>{
+
+    if(address2.value == "" || city2.value == "" || state2.value == ""  || country2.value == "" )  {
+        alert("Please enter all requied field");
+    }
+
+   if(upiId.value == ""){
+    alert("Please enter valid upi")
+   }
+   else{
+
+    setTimeout(function () {
+
+        verifyUpi.innerText = "Done 🎉"
+        verifyUpi.style.backgroundColor = "#81C784"
+
+      }, 2000)
+
+      setTimeout(function () {
+
+        payByUpi.style.visibility = "hidden";
+        h1.innerText = " Thank You for Shopping..!!!"
+
+     }, 3000)
+
+     setTimeout(function () {
+       
+        window.location.href = "../index.html"
+
+     }, 5000)
+   }
+})
+
+
+
+
+
+// ---------------   for card payment ------------------------
+
+
+let byDebitCard = document.getElementById("byDebitCard");
+let byCreditCard = document.getElementById("byCreditCard");
+
+byDebitCard.addEventListener("click",()=>{
+    PaymentDetails.style.visibility = "visible";
+    payByUpi.style.visibility = "hidden";
+})
+
+byCreditCard.addEventListener("click",()=>{
+    PaymentDetails.style.visibility = "visible";
+    payByUpi.style.visibility = "hidden";
+})
