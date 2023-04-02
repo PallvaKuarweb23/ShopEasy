0d0c36a34fcda9ea116d110516c3f50b97af5107
Hemant142 <yhemant203@gmail.com>
2023-04-02 20:27:10 +0530

    LAST

--- Scripts/index.js
+++ Scripts/index.js
@@ -6,34 +6,70 @@ let product=document.getElementById("product")
  })
 
    // Slideshow Part
-   function slideshowFun(images){
-      let counter=0;
-      setInterval(function(){
-        counter++
-      if(counter==images.length){
-        counter=0;
-      }
-     img.src=images[counter]
-      },5000);
-     
-    }
-    
-    // Use the following data for slideshow
-    var movieImages = [
-      "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/banner-images/Homepage-Banner-Beautiful-Blues_1s9JnZs.jpg?format=webp&w=1024&dpr=1.9",
-      "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/category/catban-020230216144455.jpg?format=webp&w=1024&dpr=1.9",
-      "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/banner-images/Web-Banner_1_h0DvwUm.jpg?format=webp&w=1024&dpr=1.9",
-      "https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/banner-images/Web-Banner_1_rwFtTge.jpg?format=webp&w=1024&dpr=1.9",
-      // "https://igimages.gumlet.io/hindi/gallery/movies/tubelight/tubelight_poster.jpg?w=160&dpr=2.6",
-    ]
-    
-    let slideshow=document.getElementById("slideshow");
-    let img=document.createElement("img");
-    img.src=movieImages[0];
-    slideshow.append(img);
-    
-    window.addEventListener("load", function () {
-    //   
-    slideshowFun(movieImages)
-    
-    });
+   var timeOut = 0;
+   var slideIndex = 0;
+   var autoOn = true;
+   
+   var dots = document.querySelectorAll('.dot');
+   var prevArrow = document.querySelector('.prev');
+   var showArrow = document.querySelector('.next');
+   
+   autoSlides();
+   
+   function autoSlides() {
+       timeOut = timeOut - 20;
+       if (autoOn == true && timeOut < 0) {
+           showSlides();
+       }
+       setTimeout(autoSlides, 20);
+   }
+   
+   function prevSlide() {
+   
+       timeOut = 5000;
+   
+       var slides = document.querySelectorAll(".slide");
+   
+       for (i = 0; i < slides.length; i++) {
+           slides[i].style.display = "none";
+           dots[i].className = dots[i].className.replace(" active", "");
+       }
+       slideIndex--;
+   
+       if (slideIndex > slides.length) {
+           slideIndex = 1
+       }
+       if (slideIndex == 0) {
+           slideIndex = 3
+       }
+       slides[slideIndex - 1].style.display = "block";
+       dots[slideIndex - 1].className += " active";
+   }
+   
+   function showSlides() {
+   
+       timeOut = 5000;
+   
+       var slides = document.querySelectorAll(".slide");
+   
+       for (i = 0; i < slides.length; i++) {
+           slides[i].style.display = "none";
+           dots[i].className = dots[i].className.replace(" active", "");
+       }
+       slideIndex++;
+   
+       if (slideIndex > slides.length) {
+           slideIndex = 1
+       }
+       slides[slideIndex - 1].style.display = "block";
+       dots[slideIndex - 1].className += " active";
+   }
+   
+   prevArrow.addEventListener('click', ()=> {
+       prevSlide();
+   })
+   
+   showArrow.addEventListener('click', ()=> {
+       showSlides();
+   })
+   
